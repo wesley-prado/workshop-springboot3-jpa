474947a2892588cafe540824e46375332acaf328
Wesley Prado <wesleyprado.dev@gmail.com>
2024-05-21 20:15:17 -0300

    feat: Exception handling - findById ✨

--- src/main/java/com/codemages/course/services/UserService.java
+++ src/main/java/com/codemages/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.codemages.course.services;
 
 import com.codemages.course.entities.User;
 import com.codemages.course.repositories.UserRepository;
+import com.codemages.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,7 +25,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> opt = repository.findById(id);
 
-		return opt.orElse(null);
+		return opt.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
